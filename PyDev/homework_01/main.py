0800c4a838d63f4e4e418ca851c5f9f4b31be194
elistratkin <evgeny_listrakin@mail.ru>
2022-01-12 22:41:58 +0300

    Change testing path in run-tests

--- PyDev/homework_01/main.py
+++ PyDev/homework_01/main.py
@@ -13,28 +13,21 @@ ODD = "odd"
 EVEN = "even"
 PRIME = "prime"
 
-res_list = []
-
 
 def is_prime(n):
-    prime = True
+    if n <= 1:
+        return False
     for i in range(2, n):
         if n % i == 0:
-            prime = False
-            break
-    if prime:
-        if n > 1:
-            res_list.append(n)
+            return False
+    return True
 
 
 def filter_numbers(my_list, fil):
-    for x in my_list:
-        if fil == "odd":
-            if x % 2:
-                res_list.append(x)
-        if fil == "even":
-            if x % 2 == 0:
-                res_list.append(x)
-        if fil == "prime":
-            is_prime(x)
-    return res_list
+    if fil == ODD:
+        res = list(filter(lambda x: x % 2, my_list))
+    if fil == EVEN:
+        res = list(filter(lambda x: x % 2 == 0, my_list))
+    if fil == PRIME:
+        res = list(filter(lambda x: is_prime(x), my_list))
+    return res
